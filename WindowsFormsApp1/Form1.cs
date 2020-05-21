85c2662f56ba8d466eb6fdc9ebb63203842fc007
ihehe616 <ihehe@vip.qq.com>
2020-05-21 18:35:45 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using XBE.DDD.Util;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private HookDemo hook = new HookDemo();
+
+        public Form1()
+        {
+            InitializeComponent();
+            hook.Init(DeviceChangedCallBack);
+        }
+
+
+        private int DeviceChangedCallBack(string name, bool remove)
+        {
+            if (remove)
+            {
+                listBox1.Items.Add($"Usb设备：{name}被移除");
+                return -1;
+            }
+            else
+            {
+                listBox1.Items.Add($"Usb设备：{name}已连接");
+                return 1;
+            }
+
+        }
+    }
+}
---
+++ XBE.DDD.Util/HookDemo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using CyUSB;
+
+
+namespace XBE.DDD.Util
+{
+    public class HookDemo
+    {
+        private USBDeviceList usbDevices;
+        private USBDevice device;
+
+        public delegate int OnUsbDeviceChanges(string alias, bool remove);
+
+        public OnUsbDeviceChanges DeviceChanged;
+        public int Init(OnUsbDeviceChanges deviceChangesCallBack)
+        {
+            try
+            {
+                DeviceChanged = deviceChangesCallBack;
+                usbDevices = new USBDeviceList(CyConst.DEVICES_CYUSB | CyConst.DEVICES_HID | CyConst.DEVICES_MSC);
+                //usbDevices = new USBDeviceList(0xFF);
+                usbDevices.DeviceAttached += new EventHandler(usbDevices_DeviceAttached);
+                usbDevices.DeviceRemoved += new EventHandler(usbDevices_DeviceRemoved);
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private void usbDevices_DeviceRemoved(object sender, EventArgs e)
+        {
+            USBEventArgs usbEvent = e as USBEventArgs;
+            DeviceChanged?.Invoke(usbEvent.FriendlyName, true);
+            //toolStripStatusLabel1.Text = usbEvent.FriendlyName + " removed.";
+        }
+
+        private void usbDevices_DeviceAttached(object sender, EventArgs e)
+        {
+            USBEventArgs usbEvent = e as USBEventArgs;
+            DeviceChanged?.Invoke(usbEvent.FriendlyName, false);
+            //toolStripStatusLabel1.Text = usbEvent.Device.FriendlyName + " connected.";
+
+        }
+    }
+}
